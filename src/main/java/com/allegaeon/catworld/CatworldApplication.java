939969142baf1ee149e852571ddd65d8c0d097e5
Framonteu Sarra <moshesanchez98@gmail.com>
2026-02-14 12:38:09 +0100

    chore: bootstrap project (docker mysql + flyway + profiles)

---
+++ src/main/java/com/allegaeon/catworld/CatworldApplication.java
@@ -0,0 +1,13 @@
+package com.allegaeon.catworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allegaeon/catworld/CatworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allegaeon.catworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
