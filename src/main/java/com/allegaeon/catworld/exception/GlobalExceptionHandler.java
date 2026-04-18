3bec09ac6b3a6a133f1f90d7b7b2498b93ae83f3
Framonteu Sarra <moshesanchez98@gmail.com>
2026-04-18 12:41:34 +0200

    feat: add global exception handler + resource not found exception

---
+++ src/main/java/com/allegaeon/catworld/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.allegaeon.catworld.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        for(FieldError error : exception.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+
+    }
+
+}
---
+++ src/main/java/com/allegaeon/catworld/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.allegaeon.catworld.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Object id) {
+        super(resource + " with id " + id + " not found");
+    }
+}
